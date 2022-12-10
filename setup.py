68a29d4b392f2c52212d95c63cf76eee687175ee
Carsten Richter <carichte@users.noreply.github.com>
2022-12-10 14:22:47 +0100

    added `xmltodict` to requirements

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
     install_requires=[
                       'numpy',
                       'xrayutilities',
+                      'xmltodict',
                       #'matplotlib',
                       #'scipy',
                       #'h5py',
